ed92aba801d93950b988624c2720869b40dbe916
Ton Schoots <tschoots@blackducksoftware.com>
2015-07-14 17:37:47 +0200

    get_packages.sh recognizes os version os and dkpk or rpm

--- bash/get_packages.sh
+++ bash/get_packages.sh
@@ -3,6 +3,7 @@
     
 
 
+
 ################  MAIN  #############
 
 # check the input parametes, it should be project name , release
@@ -13,13 +14,49 @@ if [ "$#" -ne 2  ]; then
    exit
 fi
 
-declare -A PCKMAP
-jsonString=""
-if [ -f /etc/debian_version ]; then
-   jsonString+="{\"bds_hub_project\":\"$1\",\"bds_hub_project_release\":\"$2\",\"ossComponentsToMatch\":["
-   jsonString+=$(dpkg -l | grep "ii" |  sed 's/ii/{"name":"/' |    awk  '{   print $1 $2 "\",\"version\":\"" $3 "\"}," ;}')
-   jsonString="${jsonString%?}"
-   jsonString+="]}"
+logfile="/tmp/$1_$2.log"
+
+date >> $logfile
+
+#determine what os we are on and if we can use dpkg or rpm
+os=$(cat /etc/os-release | grep "^ID=" | sed s/"^ID="/""/)
+version=$(cat /etc/os-release | grep "^VERSION_ID=" | sed s/"^VERSION_ID="/""/)
+dpkg=$(command -v dpkg)
+rpm=$(command -v rpm)
+echo "rpm $rpm, dpkg : $dpkg"
+package_manager="no rpm or dpkg package manager"
+if [  -z $dpkg  ];then
+   echo "rpm"
+   package_manager="rpm"
+elif [ -z $rpm   ];then
+   echo "dpkg"
+   package_manager="dpkg"
+else
+   echo "no rpm or dpkg"
 fi
 
+echo "operating system : $os, version : $version, package manager: $package_manager"
+echo "operating system : $os, version : $version, package manager: $package_manager" >> $logfile
+
+
+jsonString=""
+if [ $package_manager = "dpkg" ]; then
+   jsonString+="{\"bds_hub_project\":\"$1\",\"bds_hub_project_release\":\"$2\",\"ossComponentsToMatch\":["
+   #jsonString+=$(dpkg -l | grep "ii" |  sed 's/ii/{"name":"/' |    awk  '{   print $1 $2 "\",\"version\":\"" $3 "\"}," ;}')
+   jsonString+=$(dpkg -l | grep "ii" |  sed 's/\(^ii\s*\S*\):\S*\(\s.*\)/\1\2/' | sed 's/ii/{"name":"/' |    awk  '{   print $1 $2 "\",\"version\":\"" $3 "\"}," ;}')
+   # take the last trailing , out
+   jsonString="${jsonString%?}"
+   jsonString+="]}"
+elif [ $package_manager = "rpm" ]; then
+   echo "redhat or centos"
+   echo "fedora"
+else
+   echo "operating system not recognized"
+   echo "operating system not recognized" >> $logfile
+fi
+
+if [ -z "$jsonString"="" ]; then
+   echo "nothing generated"
+   exit
+fi
 echo $jsonString > /tmp/$1_$2.json
